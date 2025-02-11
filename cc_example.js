41b8431a79bd8fcf3cbf9c315b7f9f7a43026c9d
Annabelle Fait <imannaf8@gmail.com>
2025-02-11 16:37:19 -0500

    Task 4

--- cc_example.js
+++ cc_example.js
@@ -1,9 +1,9 @@
 // Task 1
 
-let products = [
-    {name: "Laptop", price: 1200, category: "Electronics"},
-    {name: "Notebook", price: 5, category:"Stationary"}
-];
+// let products = [
+//     {name: "Laptop", price: 1200, category: "Electronics"},
+//     {name: "Notebook", price: 5, category:"Stationary"}
+// ];
 
 // function getProductsbyCategory(products, category_name) {
 //     return products.filter(product => product.category === category_name);
@@ -11,27 +11,37 @@ let products = [
 
 // console.log(getProductsbyCategory(products, "Electronics"));
 
-// Task 2
-console.log(products)
-console.log("*".repeat(20))
-function applyDiscount(price, discountRate) {
-    return products.map(product => ({
-        ...product, // Taking apart the product objects
-        price: product.price - (product.price * discountRate)
-    }
-    ))
-}
+// // Task 2
+// console.log(products)
+// console.log("*".repeat(20))
+// function applyDiscount(price, discountRate) {
+//     return products.map(product => ({
+//         ...product, // Taking apart the product objects
+//         price: product.price - (product.price * discountRate)
+//     }
+//     ))
+// }
 
-console.log(applyDiscount(...products, 0.1));
-console.log("*".repeat(20))
-console.log(products)
+// console.log(applyDiscount(...products, 0.1));
+// console.log("*".repeat(20))
+// console.log(products)
 
 // Task 3
 
-let sales = [250, 400, 150, 900, 1200];
+// let sales = [250, 400, 150, 900, 1200];
 
-function calculateTotalRevenue(sales) {
-    return sales.reduce((total, sale)=>total+sale, 0)
-}
+// function calculateTotalRevenue(sales) {
+//     return sales.reduce((total, sale)=>total+sale, 0)
+// }
 
-console.log(`Total Rev. 2024: $${calculateTotalRevenue(sales)}`);
+// console.log(`Total Rev. 2024: $${calculateTotalRevenue(sales)}`);
+
+// Task 4
+
+let employee = {name: "John Doe", salary: 5000, position: "Manager"};
+
+function updateSalary(employee, percentageIncrease) {
+    return employee.salary += employee.salary * percentageIncrease
+};
+
+console.log(updateSalary(employee, .1));
