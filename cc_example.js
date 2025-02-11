35d9a8a59c797f855a734568de75def9c17979ca
Annabelle Fait <imannaf8@gmail.com>
2025-02-11 16:05:59 -0500

    Task 1

---
+++ cc_example.js
@@ -0,0 +1,12 @@
+// Task 1
+
+let products = [
+    {name: "Laptop", price: 1200, category: "Electronics"},
+    {name: "Notebook", price: 5, category:"Stationary"}
+];
+
+// function getProductsbyCategory(products, category_name) {
+//     return products.filter(product => product.category === category_name);
+// };
+
+// console.log(getProductsbyCategory(products, "Electronics"));
